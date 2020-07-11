f4cc47adfe95b8bb3904f9c31cda5cca86642054
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-11 22:17:53 +0530

    ADDED Test case to get custom exception when provided with wrong file path.

--- src/test/java/com/bridgelabz/iplanalysertest/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalysertest/IPLAnalyserTest.java
@@ -7,10 +7,12 @@ import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IPLAnalyserTest
 {
     private static final String IPL_RUN_CSV_FILE_PATH = "./src/test/resources/Day16 Data_01 IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_RUN_CSV_WRONG_FILE_PATH = "./src/main/resources/Day16 Data_01 IPL2019FactsheetMostRuns.csv";
     private static final String IPL_WICKETS_CSV_FILE_PATH = "./src/test/resources/Day16 Data_02 IPL2019FactsheetMostWkts.csv";
     IPLAnalyser iplMostRuns;
     IPLAnalyser oplMostWickets;
@@ -19,6 +21,8 @@ public class IPLAnalyserTest
     {
         iplMostRuns = new IPLAnalyser(IPLAnalyser.Player.BATSMAN);
         oplMostWickets = new IPLAnalyser(IPLAnalyser.Player.BOWLER);
+        ExpectedException exceptionRule = ExpectedException.none();
+        exceptionRule.expect(IPLAnalyserException.class);
     }
 
     @Test
@@ -28,6 +32,18 @@ public class IPLAnalyserTest
         {
             int iplRecords = iplMostRuns.loadIPLData(IPLAnalyser.Player.BATSMAN, ',', IPL_RUN_CSV_FILE_PATH);
             Assert.assertEquals(100, iplRecords);
+        } catch (IPLAnalyserException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenWrongIPLMostRunsCSVFile_ShouldThrowCustomException()
+    {
+        try
+        {
+            iplMostRuns.loadIPLData(IPLAnalyser.Player.BATSMAN, ',', IPL_RUN_CSV_WRONG_FILE_PATH );
         } catch (IPLAnalyserException e)
         {
             e.printStackTrace();
